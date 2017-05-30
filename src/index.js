7eb408e525d3ee030b4108290a99743738d0e96d
Manan V. Panchal <mpanchal@integconsulting.com>
2017-05-30 20:28:29 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Table from './table';
+import * as d3 from 'd3';
 
-ReactDOM.render(<div><Table/></div>, document.getElementById('app'));
+let dataSet = new Array(15).fill(undefined).map(d => {
+  return Math.random() * 100;
+});
+
+let w = 400; let h = 300;
+
+let svg = d3.select('#app').append('svg')
+.attr('width', w)
+.attr('height', h);
+
+let mult = 8;
+
+let yScale = d3.scaleLinear()
+.domain([0, d3.max(dataSet) * 1.1])
+.range([0, h]);
+
+let xScale = d3.scaleBand()
+.domain(dataSet)
+.range([0, w], 0.2);
+
+svg.selectAll('rect')
+.data(dataSet)
+.enter()
+.append('rect')
+.attr('style', 'fill: teal')
+.attr('x', (d, i) => {
+  return xScale(d);
+})
+.attr('y', (d, i) => {
+  return h - yScale(d);
+})
+.attr('width', xScale.bandwidth() * 0.9)
+.attr('height', (d, i) => {
+  return yScale(d);
+});
